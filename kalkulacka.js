82f1086a356da3fa390b0b178923fe5fe4774408
kuri1z <mynamenoname@post.cz>
2025-11-04 11:02:41 +0100

    sss

--- kalkulacka.js
+++
@@ -1,6 +0,0 @@
-let display = document.getElementById('display');
-document.querySelectorAll('button').forEach(btn => btn.onclick = () => {
-  if (btn.textContent == 'C') display.value = '';
-  else if (btn.textContent == '=') display.value = eval(display.value || '0');
-  else display.value += btn.textContent;
-});
